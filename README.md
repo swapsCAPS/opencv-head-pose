a9e3f049183531fd7eca27bdba557a4d1df160ee
swapsCAPS <verstegen.daan@gmail.com>
2019-07-12 12:12:31 +0200

    removed dlib window, draw dlib landmarks using opencv

---
+++ README.md
@@ -0,0 +1,7 @@
+# Playing around with dlib and opencv
+
+### resources and credits
+- http://dlib.net
+- https://opencv.org/
+- https://www.learnopencv.com/head-pose-estimation-using-opencv-and-dlib/
+- https://www.pyimagesearch.com/2017/04/03/facial-landmarks-dlib-opencv-python/
--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import os
 import cv2 as cv
 import dlib
 import time
+import threading
 
-cap      = cv.VideoCapture(0)
-detector = dlib.get_frontal_face_detector()
+cap            = cv.VideoCapture(0)
+detector       = dlib.get_frontal_face_detector()
 predictor_path = "{}/Downloads/shape_predictor_68_face_landmarks.dat".format(os.environ['HOME'])
-predictor = dlib.shape_predictor(predictor_path)
-win = dlib.image_window()
+predictor      = dlib.shape_predictor(predictor_path)
 
-draw_color = dlib.rgb_pixel(255,0,0)
+facial_landmark_indices = {
+    "eye_left":    46,
+    "eye_right":   36,
+    "mouth_left":  54,
+    "mouth_right": 48,
+    "nose_tip":    30,
+    "chin":        8,
+}
+
+draw_color = dlib.rgb_pixel(0,255,0)
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -25,15 +34,21 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
-    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-
     detected_faces = detector(frame, 1)
 
-    objects = list(map(lambda pos: predictor(frame, pos), detected_faces))
+    for face in detected_faces:
+        shape     = predictor(frame, face)
+        landmarks = map(lambda idx: shape.part(idx) , facial_landmark_indices.values())
 
-    win.clear_overlay()
-    win.set_image(frame)
-    win.add_overlay(objects=objects)
+        for landmark in landmarks:
+            print('landmark', landmark)
+
+            cv.circle(frame, (landmark.x, landmark.y), 3, (0,255,0), -1)
+
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
 
 # When everything done, release the capture
 cap.release()
